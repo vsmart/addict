8fb4a0ae6b2a342c2ffabdb2227b8782598aac0a
Franziska Schmidt <hello@franka.tech>
2017-09-12 14:20:29 +0200

    Comment out stuff to make redirect work

--- lib/addict/controller.ex
+++ lib/addict/controller.ex
@@ -123,9 +123,9 @@ defmodule Addict.AddictController do
 
   defp return_success(conn, user, custom_fn, status \\ 200) do
     conn
-    |> put_status(status)
+    #    |> put_status(status)
     |> invoke_hook(custom_fn, :ok, user)
-    |> json(Addict.Presenter.strip_all(user))
+    #    |> json(Addict.Presenter.strip_all(user))
     |> redirect(to: "/")
   end
 
